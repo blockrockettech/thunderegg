9ac8130b3164195dd3904018f828200c2f597767
Andy Gray <andy.gray@blockrocket.tech>
2020-11-05 12:57:13 +0000

    dropped frontend

--- thunderegg-frontend/src/utils.js
+++
@@ -1,9 +0,0 @@
-export function getContractAddressFromConf(truffleConf, chainId) {
-  if (!truffleConf || !chainId) return "";
-  const { networks } = truffleConf;
-  if (networks[chainId.toString()]) {
-    const address = networks[chainId.toString()].address;
-    return address ? address : "";
-  }
-  return "";
-}
